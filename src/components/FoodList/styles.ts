9c1badfe0bc1b33317d8d17cf906224ee96f2973
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-08-28 18:27:36 -0300

    Atualizando o projeto com a parte 3 e a responsividade até o momento concluída

--- src/components/FoodList/styles.ts
+++ src/components/FoodList/styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
-import { cores } from '../../styles'
-import { AddCartButton } from '../Food/styles'
+import { breakpoints, cores } from '../../styles'
 
 export const Container = styled.div`
   max-width: 1024px;
@@ -8,12 +7,25 @@ export const Container = styled.div`
   li {
     list-style: none;
   }
+  @media (max-width: ${breakpoints.tablet}) {
+    margin: 20px auto;
+  }
 `
 export const List = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 32px;
   row-gap: 32px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    grid-template-columns: 1fr 1fr;
+    margin: 0 10px;
+  }
+  @media (max-width: ${breakpoints.mobile}) {
+    grid-template-columns: 1fr;
+    margin: 0 10px;
+    row-gap: 10px;
+  }
 `
 
 export const Modal = styled.div`
@@ -50,16 +62,20 @@ export const ModalContent = styled.div`
   position: relative;
   z-index: 3;
 
-  ${AddCartButton} {
-    margin: 0;
+  @media (max-width: ${breakpoints.tablet}) {
+    padding: 15px;
   }
 `
 export const FoodImage = styled.img`
   object-fit: cover;
-  width: 280px;
-  height: 280px;
+  max-width: 280px;
+  max-height: 280px;
   margin: 32px 24px 32px 32px;
   object-fit: cover;
+
+  @media (max-width: ${breakpoints.mobile}) {
+    display: none;
+  }
 `
 export const ModalContainer = styled.div`
   max-width: 656px;
@@ -90,5 +106,16 @@ export const CloseIcon = styled.img`
   max-height: 16px;
   width: 100%;
   margin: 8px;
+  cursor: pointer;
+`
+export const AddCartButton = styled.button`
+  background-color: ${cores.rosa};
+  color: ${cores.vermelho};
+  border: none;
+  padding: 4px 7px;
+  font-weight: 700;
+  text-decoration: none;
+  text-align: center;
+  font-size: 14px;
   cursor: pointer;
 `
